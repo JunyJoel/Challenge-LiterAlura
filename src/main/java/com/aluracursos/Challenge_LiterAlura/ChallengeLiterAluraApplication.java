a1e9a294f6204900f17cf492b459dbf2a475d4c1
JunyJoel <juny.joel@gmail.com>
2025-06-17 11:46:34 -0600

    ingresando proyecto spring a Git

---
+++ src/main/java/com/aluracursos/Challenge_LiterAlura/ChallengeLiterAluraApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.Challenge_LiterAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiterAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiterAluraApplication.class, args);
+	}
+
+}
